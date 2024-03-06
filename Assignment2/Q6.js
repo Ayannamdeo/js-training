1270546aa5b1f44fcee1fb6b4d1c4ec10e5e2172
Ayan Namdeo <ayan.namdeo@ayannamdeor-424.successivetech.com>
2024-03-06 18:30:33 +0530

    assignment 2 completed

---
+++ Assignment2/Q6.js
@@ -0,0 +1,8 @@
+//Write a function expression that takes in a number and returns true if it's even and false if it's odd.
+
+function oddEven(n){
+    return n%2 === 0;
+}
+
+console.log(oddEven(8));
+console.log(oddEven(5));
